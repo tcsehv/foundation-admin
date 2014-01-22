ce0a9ece990f2093e63358e5584f9f9cf8cc985f
Teun van Heerebeek <t.van.heerebeek@theconceptstore.nl>
2014-01-22 10:49:50 +0100

    Added: - Tooltips - Select2 - Off canvas switch button for >= 1600px screens - Modal operation buttons (save/cancel) - Progress modal
    Styling:
    - Icons in alert boxes
    - Dropdown button
    - Font sizes / spacing
    - Breadcrumbs
    - Modal
    
    Misc:
    - Minified & compressed JS-files to a single file
    - Optimized Sass files
    - Optimized HTML file (Dashboard_1.html)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
+// Do not collapse the off-canvas menu if the screen >= 1600px
+if(window.innerWidth >= 1600) {
+    $("div.off-canvas-wrap").addClass("move-right big");
+    $("#menu-collapse-btn").show();
+}
+
 $(document).ready( function() {
+    // Do not collapse the off-canvas menu if the screen is resized to >= 1600px
+    $(window).on("resize", function() {
+        if(window.innerWidth >= 1600) {
+            $("div.off-canvas-wrap").addClass("move-right big");
+            $("#menu-collapse-btn").show();
+        }
+        else {
+            $("div.off-canvas-wrap").removeClass("move-right big");
+            $("#menu-collapse-btn").hide();
+        }
+    });
+
+    // Menu collapse switcher
+    $("#menu-collapse-btn").on("click", function(){
+        var content = $("div.off-canvas-wrap");
+        if (content.hasClass("move-right big")){
+            content.removeClass("move-right big");
+            $(this).find("i").removeClass("fi-arrows-out").addClass("fi-arrows-in");
+        }
+        else {
+            content.addClass("move-right big");
+            $(this).find("i").removeClass("fi-arrows-in").addClass("fi-arrows-out");
+        }
+    });
+
     // Stretch Foundation Off canvas to full height of the page
     $('a.left-off-canvas-toggle').click(function() {
         $('.inner-wrap').css('min-height', $(window).height() + 'px');
@@ -21,9 +52,9 @@ $(document).ready( function() {
 
     // Back to top button
     $('#top').on('click', function () {
-        $('body').animate({
+        $('body,html').animate({
             scrollTop: 0
-        }, 800);
+        }, 400);
         return false;
     });
 
@@ -36,7 +67,7 @@ $(document).ready( function() {
             'data' : [
                 { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
                 { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
-                { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1", "type" : "page"},
+                { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1", "type" : "page" },
                 { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2", "type" : "page" },
                 { "id" : "ajson5", "parent" : "ajson2", "text" : "Child 3", "type" : "media" }
             ],
@@ -64,9 +95,24 @@ $(document).ready( function() {
         //console.log(data.selected);
     });
 
-    // Interact with the tree
+    // Interact with the JStree
     $('#tree').jstree(true).select_node('child_node');
 
-    // End JStree
+    // Modal
+    $("body").on('click', "#close-modal", function(){
+        $('#modal').foundation('reveal', 'close');
+        return false;
+    });
+    $("body").on('click', "#progress-modal-btn", function(){
+        $(this).css("opacity", ".6").find('i').removeClass("fi-check").addClass("fi-loop");
+        setTimeout(function () {
+            $('#progress-modal').foundation('reveal', 'open');
+        }, 4000);
+        return false;
+    });
 
+    // Select2
+    $("#select2example").select2({
+        placeholder: "Select a country"
+    });
 });
